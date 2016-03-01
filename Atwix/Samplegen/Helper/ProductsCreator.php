8d68493057cc3cae8c4eeb8e6b5b381da3eb32c7
Yaroslav Rogoza <rogyar@gmail.com>
2016-03-01 12:01:46 +0200

    Fixed issue with absent simple product attribute. Minor refactorings

--- Atwix/Samplegen/Helper/ProductsCreator.php
+++ Atwix/Samplegen/Helper/ProductsCreator.php
@@ -4,7 +4,6 @@ namespace Atwix\Samplegen\Helper;
 
 use Magento\Catalog\Api\Data\ProductExtensionFactory;
 use Magento\Catalog\Model\CategoryFactory;
-use Magento\CatalogInventory\Api\StockItemRepositoryInterface;
 use Magento\CatalogWidget\Model\Rule\Condition\ProductFactory;
 use Magento\ConfigurableProduct\Api\Data\OptionInterface;
 use Magento\ConfigurableProduct\Api\Data\OptionValueInterface;
@@ -15,6 +14,7 @@ use \Atwix\Samplegen\Console\Command\GenerateProductsCommand;
 use Magento\Catalog\Model\Product\Type as Type;
 use Magento\Catalog\Api\ProductAttributeRepositoryInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 // TODO refactor for ability to use abstract generator class
 // TODO add a separate atribute instead a name prefix for all items
@@ -60,7 +60,7 @@ class ProductsCreator extends \Magento\Framework\App\Helper\AbstractHelper
      * @var \Magento\Catalog\Model\CategoryFactory
      */
     protected $categoryFactory;
-    
+
 
     protected $processedProducts = 0;
 
@@ -131,6 +131,9 @@ class ProductsCreator extends \Magento\Framework\App\Helper\AbstractHelper
         }
     }
 
+    /**
+     * Inits product generation process
+     */
     public function createProducts()
     {
         if ($this->getCount() > 1) {
@@ -152,6 +155,14 @@ class ProductsCreator extends \Magento\Framework\App\Helper\AbstractHelper
 
     }
 
+    /**
+     * Creates a simple product
+     *
+     * @param bool|false $forceDefaultCategory
+     * @param bool|false $doNotSave
+     * @return \Magento\Catalog\Model\Product
+     * @throws \Exception
+     */
     public function createSimpleProduct($forceDefaultCategory = false, $doNotSave = false)
     {
         /** @var \Magento\Catalog\Model\Product $product */
@@ -191,50 +202,25 @@ class ProductsCreator extends \Magento\Framework\App\Helper\AbstractHelper
         return $product;
     }
 
+    /**
+     *
+     * Creates a configurable product with child simple products
+     *
+     * @throws \Exception
+     */
     public function createConfigurableProduct()
     {
         if ($this->processedProducts >= $this->getCount()) {
             return;
         }
 
-        // Process configurable options
-
         $configurableAttribute = $this->attributeRepository->get(self::CONFIGURABLE_ATTRIBUTE);
-        $availableOptions = $configurableAttribute->getOptions();
 
-        // TODO: check if options are available
-
-        // Create child simple products
-        $availableProductsCount = $this->getCount() - $this->processedProducts - 1;
-
-        if ($availableProductsCount >= self::CONFIGURABLE_CHILD_LIMIT) {
-            $childrenLimit = self::CONFIGURABLE_CHILD_LIMIT;
-        } else {
-            $childrenLimit = $availableProductsCount;
+        if (null == $configurableAttribute) {
+            throw new \Exception("Selected configurable attribute {self::CONFIGURABLE_ATTRIBUTE} is not available");
         }
 
-        if ($childrenLimit > count($availableOptions)) {
-            $childrenLimit = count($availableOptions);
-        }
-
-        $childrenCount = rand(1, $childrenLimit);
-        $availableOptionsKeys = array_rand($availableOptions, $childrenCount);
-
-        $childProductsIds = $configurableOptionsValues = [];
-
-        for($optCount = 0; $optCount < $childrenCount; $optCount++ ) {
-            $product = $this->createSimpleProduct(true, true);
-            $optValueId =  is_array($availableOptionsKeys) ?
-                $availableOptions[$availableOptionsKeys[$optCount]]->getValue() :
-                $availableOptions[$availableOptionsKeys]->getValue();
-            // FIXME: sometimes the color attribute value is empty - causes an error
-            $product->setCustomAttribute($configurableAttribute->getAttributeCode(), $optValueId);
-            $optionValue = $this->optionValue;
-            $optionValue->setValueIndex($optValueId);
-            $configurableOptionsValues[] = $optionValue;
-            $product = $this->productRepository->save($product);
-            $childProductsIds[] = $product->getId();
-        }
+        $childProductsData = $this->createConfigurableChildren($configurableAttribute);
 
         /** @var \Magento\Catalog\Model\Product $configurableProduct */
         $configurableProduct = $this->objectManager->create('Magento\Catalog\Model\Product');
@@ -251,14 +237,14 @@ class ProductsCreator extends \Magento\Framework\App\Helper\AbstractHelper
         $configurableOption = $this->configurableOption;
         $configurableOption->setAttributeId($configurableAttribute->getAttributeId())
             ->setLabel('Color')
-            ->setValues($configurableOptionsValues);
+            ->setValues($childProductsData['configurable_options_values']);
 
         $extensionAttributes = $configurableProduct->getExtensionAttributes();
         if (!$extensionAttributes) {
             $extensionAttributes = $this->productExtensionFactory->create();
         }
 
-        $extensionAttributes->setConfigurableProductLinks($childProductsIds);
+        $extensionAttributes->setConfigurableProductLinks($childProductsData['child_products_ids']);
         $extensionAttributes->setConfigurableProductOptions([$configurableOption]);
         $configurableProduct->setExtensionAttributes($extensionAttributes);
 
@@ -267,6 +253,13 @@ class ProductsCreator extends \Magento\Framework\App\Helper\AbstractHelper
         $this->processedProducts++;
     }
 
+    /**
+     * Returns a random product category
+     *
+     * @return \Magento\Framework\DataObject
+     * @throws \Exception
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
     protected function getProductCategory()
     {
         if (NULL == $this->availableCategories) {
@@ -284,11 +277,77 @@ class ProductsCreator extends \Magento\Framework\App\Helper\AbstractHelper
         }
     }
 
+    /**
+     * Generates child products for configurable product
+     *
+     * @param \Magento\Catalog\Api\Data\ProductAttributeInterface $configurableAttribute
+     * @return array
+     * @throws \Exception
+     */
+    protected function createConfigurableChildren($configurableAttribute)
+    {
+        $availableOptions = $configurableAttribute->getOptions();
+
+        /* Not sure why the zero index has no value for all attributes. Maybe will be fixed
+           in next Magento versions */
+        unset($availableOptions[0]);
+
+        if (!count($availableOptions) > 0) {
+            throw new \Exception('The selected configurable attribute has no values');
+        }
+        // Create child simple products
+        $availableProductsCount = $this->getCount() - $this->processedProducts - 1;
+
+        if ($availableProductsCount >= self::CONFIGURABLE_CHILD_LIMIT) {
+            $childrenLimit = self::CONFIGURABLE_CHILD_LIMIT;
+        } else {
+            $childrenLimit = $availableProductsCount;
+        }
+
+        if ($childrenLimit > count($availableOptions)) {
+            $childrenLimit = count($availableOptions);
+        }
+
+        $childrenCount = rand(1, $childrenLimit);
+        $childProductsIds = $configurableOptionsValues = [];
+
+        for($optCount = 0; $optCount < $childrenCount; $optCount++ ) {
+            $product = $this->createSimpleProduct(true, true);
+            $currentOptionId = array_rand($availableOptions);
+            $optValueId = $availableOptions[$currentOptionId]->getValue();
+            unset($availableOptions[$currentOptionId]);
+            echo "$currentOptionId Value id is $optValueId \n";
+            //var_dump(array_keys($availableOptions));
+
+            $product->setCustomAttribute($configurableAttribute->getAttributeCode(), $optValueId);
+            $optionValue = $this->optionValue;
+            $optionValue->setValueIndex($optValueId);
+            $configurableOptionsValues[] = $optionValue;
+            $product = $this->productRepository->save($product);
+            $childProductsIds[] = $product->getId();
+        }
+
+        return [
+            'child_products_ids'            => $childProductsIds,
+            'configurable_options_values'   => $configurableOptionsValues
+        ];
+    }
+
+    /**
+     * Returns products number to generate
+     *
+     * @return mixed
+     */
     protected function getCount()
     {
         return $this->parameters['count'];
     }
 
+    /**
+     * Removes all generated products
+     *
+     * @return bool
+     */
     protected function removeGeneratedItems()
     {
         /** @var \Magento\Catalog\Model\Product $product */
